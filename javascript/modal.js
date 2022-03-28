46d9b4796bbe98bce8479fa54bc988845a098f6a
Naveenchandar <naveenchandece@gmail.com>
2022-03-28 07:34:36 +0530

    seperate js file for modal toggle

---
+++ javascript/modal.js
@@ -0,0 +1,16 @@
+const modal = document.getElementById("modal_container");
+const openBtn = document.getElementById("modal_open_btn");
+const closeBtn = document.getElementById("modal_close_btn");
+openBtn.onclick = function () {
+    modal.style.display = "block";
+}
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
