e1bdf7a59d25c7fa907f428a0b796243055cb8a0
Naveenchandar <naveenchandece@gmail.com>
2022-06-18 09:53:34 +0530

    modal ok button onclick changes

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,10 +1,14 @@
 const modal = document.getElementById("modal_container");
 const openBtn = document.getElementById("modal_open_btn");
 const closeBtn = document.getElementById("modal_close_btn");
+const okBtn = document.getElementById("modal_ok_btn");
 openBtn.onclick = function () {
     modal.style.display = "block";
 }
 closeBtn.onclick = function () {
+    modal.style.display = "none";
+}
+okBtn.onclick = function () {
     modal.style.display = "none";
 }
 
